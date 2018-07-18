c66269bd2e4f3727f8b4ba8064946811d4d85a64
Nick Robinson <npr251@gmail.com>
2018-07-18 16:49:24 +0100

    Rename path -> episode

--- softqlearning/misc/sampler.py
+++ softqlearning/misc/sampler.py
@@ -4,21 +4,21 @@ import time
 from . import logger
 
 
-def rollout(env, policy, path_length, render=False, speedup=None):
+def rollout(env, policy, episode_length, render=False, speedup=None):
     Da = env.action_space.n
     Do = np.prod(env.observation_space.shape)
 
     observation = env.reset()
 
-    observations = np.zeros((path_length + 1, Do))
-    actions = np.zeros((path_length, Da))
-    dones = np.zeros((path_length, ))
-    rewards = np.zeros((path_length, ))
+    observations = np.zeros((episode_length + 1, Do))
+    actions = np.zeros((episode_length, Da))
+    dones = np.zeros((episode_length, ))
+    rewards = np.zeros((episode_length, ))
     agent_infos = []
     env_infos = []
 
     t = 0
-    for t in range(path_length):
+    for t in range(episode_length):
         action, agent_info = policy.get_action(observation)
         next_obs, reward, done, env_info = env.step(action)
 
@@ -42,7 +42,7 @@ def rollout(env, policy, path_length, render=False, speedup=None):
 
     observations[t + 1] = observation
 
-    path = {
+    episode = {
         'observations': observations[:t + 1],
         'actions': actions[:t + 1],
         'rewards': rewards[:t + 1],
@@ -52,20 +52,20 @@ def rollout(env, policy, path_length, render=False, speedup=None):
         'env_infos': env_infos
     }
 
-    return path
+    return episode
 
 
-def rollouts(env, policy, path_length, n_paths):
-    paths = list()
-    for i in range(n_paths):
-        paths.append(rollout(env, policy, path_length))
+def rollouts(env, policy, episode_length, n_episodes):
+    episodes = list()
+    for i in range(n_episodes):
+        episodes.append(rollout(env, policy, episode_length))
 
-    return paths
+    return episodes
 
 
 class Sampler(object):
-    def __init__(self, max_path_length, min_replay_buffer_size, batch_size):
-        self._max_path_length = max_path_length
+    def __init__(self, max_episode_length, min_replay_buffer_size, batch_size):
+        self._max_episode_length = max_episode_length
         self._min_replay_buffer_size = min_replay_buffer_size
         self._batch_size = batch_size
 
@@ -99,10 +99,10 @@ class SimpleSampler(Sampler):
     def __init__(self, **kwargs):
         super(SimpleSampler, self).__init__(**kwargs)
 
-        self._path_length = 0
-        self._path_return = 0
-        self._last_path_return = 0
-        self._max_path_return = -np.inf
+        self._episode_length = 0
+        self._episode_return = 0
+        self._last_episode_return = 0
+        self._max_episode_return = -np.inf
         self._n_episodes = 0
         self._current_observation = None
         self._total_samples = 0
@@ -113,8 +113,8 @@ class SimpleSampler(Sampler):
 
         action, _ = self.policy.get_action(self._current_observation)
         next_observation, reward, done, info = self.env.step(action)
-        self._path_length += 1
-        self._path_return += reward
+        self._episode_length += 1
+        self._episode_return += reward
         self._total_samples += 1
 
         self.replay_buffer.add_sample(
@@ -124,14 +124,14 @@ class SimpleSampler(Sampler):
             done=done,
             next_observation=next_observation)
 
-        if done or self._path_length >= self._max_path_length:
+        if done or self._episode_length >= self._max_episode_length:
             self._current_observation = self.env.reset()
-            self._path_length = 0
-            self._max_path_return = max(self._max_path_return,
-                                        self._path_return)
-            self._last_path_return = self._path_return
+            self._episode_length = 0
+            self._max_episode_return = max(self._max_episode_return,
+                                        self._episode_return)
+            self._last_episode_return = self._episode_return
 
-            self._path_return = 0
+            self._episode_return = 0
             self._n_episodes += 1
 
         else:
@@ -139,16 +139,16 @@ class SimpleSampler(Sampler):
 
     def log_diagnostics(self):
         super(SimpleSampler, self).log_diagnostics()
-        logger.record_tabular('max-path-return', self._max_path_return)
-        logger.record_tabular('last-path-return', self._last_path_return)
+        logger.record_tabular('max-episode-return', self._max_episode_return)
+        logger.record_tabular('last-episode-return', self._last_episode_return)
         logger.record_tabular('episodes', self._n_episodes)
         logger.record_tabular('total-samples', self._total_samples)
 
 
 class DummySampler(Sampler):
-    def __init__(self, batch_size, max_path_length):
+    def __init__(self, batch_size, max_episode_length):
         super(DummySampler, self).__init__(
-            max_path_length=max_path_length,
+            max_episode_length=max_episode_length,
             min_replay_buffer_size=0,
             batch_size=batch_size)
 
